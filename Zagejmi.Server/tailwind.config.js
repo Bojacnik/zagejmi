a93a8dbdcdd07b28a9a3d2285cabe6077a6ce540
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-02-16 17:25:36 +0100

    fix: removed useless lookup for tailwind

--- Zagejmi.Server/tailwind.config.js
+++ Zagejmi.Server/tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
     mode: 'jit',
     content: [
         "./**/*.{razor,html,cshtml}",
-        "../BojaWeb.Client/**/*.{razor,html,cshtml}",
     ],
     theme: {
         extend: {},
