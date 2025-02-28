6fc8bf3dcd512edb74e6cb356c9502e5fb0fa932
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-02-28 22:02:20 +0100

    feat: added uniqueness to email via fluent api

---
+++ Zagejmi.Infrastructure/Ctx/ZagejmiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zagejmi.Infrastructure.Models;
+
+namespace Zagejmi.Infrastructure.Ctx;
+
+public class ZagejmiContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure PersonalInfo
+        modelBuilder.Entity<PersonalInformationModel>()
+            .HasIndex(i => i.Email)
+            .IsUnique();
+    }
+}
