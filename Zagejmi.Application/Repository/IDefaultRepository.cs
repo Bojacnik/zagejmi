83fc6fb0931cfab0e3d502882a857b1e178bf4f9
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-02-28 22:04:17 +0100

    feat: added interface for default repository

---
+++ Zagejmi.Application/Repository/IDefaultRepository.cs
@@ -0,0 +1,15 @@
+﻿using Zagejmi.Domain;
+using Zagejmi.Domain.Events;
+
+namespace Zagejmi.Application.Repository;
+
+public interface IDefaultRepository<TEntity, TEvent>
+    where TEntity : Entity<TEvent> where TEvent : IDomainEvent
+{
+    Task<ICollection<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+    Task<TEntity> GetByIdAsync(ulong id, CancellationToken cancellationToken);
+    void AddAsync(TEntity entity, CancellationToken cancellationToken);
+    void UpdateAsync(TEntity oldEntity, TEntity newEntity, CancellationToken cancellationToken);
+    void DeleteAsync(ulong id, CancellationToken cancellationToken);
+    Task<bool> FlushAsync(CancellationToken cancellationToken);
+}
