a8f6a98acea7065dc0048ed0f0796870833cf7d7
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-02-28 21:58:04 +0100

    feat: added models to infrastructure

---
+++ Zagejmi.Infrastructure/Models/PersonStatsModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zagejmi.Infrastructure.Models;
+
+[Table("PersonStats")]
+public class PersonStatsModel
+{
+    [Key] public ulong Id { get; set; }
+
+    public ulong TotalScore { get; set; }
+    public ulong Level { get; set; }
+
+    public ulong TimeTotal { get; set; }
+    public ulong TimeWatching { get; set; }
+
+    public uint ChatsSent { get; set; }
+
+    public ulong CzkSpent { get; set; }
+    public ulong GoingSpent { get; set; }
+    public ulong TransactionAmount { get; set; }
+    public ulong GiftsSent { get; set; }
+}
