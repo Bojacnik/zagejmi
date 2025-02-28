6c8fedf7bf45275b857135762b3bcad1c77a927f
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-02-28 22:06:24 +0100

    feat: added person repository

---
+++ Zagejmi.Infrastructure/Repository/PersonRepository.cs
@@ -0,0 +1,51 @@
+﻿using Zagejmi.Application.Repository;
+using Zagejmi.Domain.Community.User;
+using Zagejmi.Infrastructure.Ctx;
+using Zagejmi.Infrastructure.Models;
+
+namespace Zagejmi.Infrastructure.Repository;
+
+public class PersonRepository(ZagejmiContext dbContext) : IPersonRepository
+{
+    private ZagejmiContext _dbContext = dbContext;
+    
+    public Task<ICollection<Person>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Person> GetByIdAsync(ulong id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddAsync(Person entity, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateAsync(Person oldEntity, Person newEntity, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAsync(ulong id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> FlushAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Person> GetByEmailAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Person> GetByUsernameAsync(string username)
+    {
+        throw new NotImplementedException();
+    }
+}
