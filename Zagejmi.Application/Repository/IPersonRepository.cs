9d00e333672d4c21c898e7859953dc7ed6a73d53
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-02-28 22:05:15 +0100

    feat: added IPersonRepository

---
+++ Zagejmi.Application/Repository/IPersonRepository.cs
@@ -0,0 +1,10 @@
+﻿using Zagejmi.Domain.Community.User;
+using Zagejmi.Domain.Events;
+
+namespace Zagejmi.Application.Repository;
+
+public interface IPersonRepository : IDefaultRepository<Person, IPersonEvent>
+{
+    Task<Person> GetByEmailAsync(string email);
+    Task<Person> GetByUsernameAsync(string username);
+}
