11b59767533107e2a352201b8a50bac3dd5652aa
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-02-28 22:06:37 +0100

    feat: added event store

---
+++ Zagejmi.Infrastructure/EventStore/EventStore.cs
@@ -0,0 +1,16 @@
+﻿using Zagejmi.Application.EventStore;
+using Zagejmi.Domain.Events;
+
+namespace Zagejmi.Infrastructure.EventStore;
+
+public class EventStore(Microsoft.EntityFrameworkCore.DbContext context) : IEventStore
+{
+    public async Task SaveEventAsync<T>(
+        T @event,
+        CancellationToken cancellationToken
+    ) where T : class, IDomainEvent
+    {
+        await context.Set<T>().AddAsync(@event, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
