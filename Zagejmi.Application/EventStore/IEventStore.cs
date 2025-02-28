d9084674abe8a250cd44696f9efe57569b0eadf8
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-02-28 22:02:56 +0100

    feat: added IEventStore

---
+++ Zagejmi.Application/EventStore/IEventStore.cs
@@ -0,0 +1,9 @@
+﻿using Zagejmi.Domain.Events;
+
+namespace Zagejmi.Application.EventStore;
+
+public interface IEventStore
+{
+    Task SaveEventAsync<T>(T @event, CancellationToken cancellationToken) 
+        where T : class, IDomainEvent;
+}
