b23ba0c96fba67cac9cb04223f4303da289f50ba
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-03-03 22:51:08 +0100

    chore: renamed Transaction to GoinTransaction

---
+++ Zagejmi.Application/Events/GoinTransactions/GoinTransactionUpdatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using Zagejmi.Domain.Events;
+
+namespace Zagejmi.Application.Events.GoinTransactions;
+
+public class GoinTransactionUpdated(
+    Guid eventId,
+    DateTime timestamp,
+    ulong version,
+    Guid aggregateId,
+    string eventType)
+    : IGoinTransactionEvent
+{
+    public Guid EventId { get; } = eventId;
+    public DateTime Timestamp { get; } = timestamp;
+    public ulong Version { get; } = version;
+    public Guid AggregateId { get; } = aggregateId;
+    public string EventType { get; } = eventType;
+}
