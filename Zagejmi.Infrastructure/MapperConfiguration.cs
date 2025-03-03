36e09833ebab8efbee34d5e2270e0bc659e408df
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-03-03 14:35:54 +0100

    chore: updated mappings

--- Zagejmi.Infrastructure/MapperConfiguration.cs
+++ Zagejmi.Infrastructure/MapperConfiguration.cs
@@ -1,18 +1,29 @@
-﻿using AutoMapper;
+﻿using System.Transactions;
+using AutoMapper;
 using Zagejmi.Domain.Community.User;
 using Zagejmi.Infrastructure.Models;
 
 namespace Zagejmi.Infrastructure;
 
-public class OrganizationProfile : Profile
+public class MapperPersonProfile : Profile
 {
-    public OrganizationProfile()
+    public MapperPersonProfile()
     {
         // TODO: test this to check if ReverseMap doesn't fail
         CreateMap<Person, PersonModel>()
             .ForMember(x => x.GoinWalletId, opt => opt.Ignore())
             .ForMember(x => x.PersonalInformationId, opt => opt.Ignore())
             .ForMember(x => x.StatisticsId, opt => opt.Ignore())
+            .ReverseMap();
+    }
+}
+
+public class MapperGoinTransactionProfile : Profile
+{
+    public MapperGoinTransactionProfile()
+    {
+        // TODO: ignore fields that are unmappable
+        CreateMap<Transaction, GoinTransactionModel>()
             .ReverseMap();
     }
 }
