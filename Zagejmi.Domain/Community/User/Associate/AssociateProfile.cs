ca9d44678cc8c71ee62cd0b77e2ebfb9c8aba1a3
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2025-03-09 14:07:14 +0100

    refactor: moved associate profile to its own folder, also added card image

---
+++ Zagejmi.Domain/Community/User/Associate/AssociateProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Zagejmi.Domain.Community.User.Associate;
+
+public record AssociateProfile(Image CardProfilePicture)
+{
+    public required Person Owner;
+
+    public Image CardProfilePicture = CardProfilePicture;
+
+    // TODO: Add a lot of stuff :) (i.e personality, astro-sign, badges, animals, photos, ...)
+}
