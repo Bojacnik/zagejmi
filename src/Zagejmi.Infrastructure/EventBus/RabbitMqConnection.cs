d979f93e27cdd5d918c08569b8e4b6ed2441134c
Vít Němec <vitek2004@seznam.cz>
2025-07-01 06:13:13 +0200

    feat: added RabbitMq

---
+++ src/Zagejmi.Infrastructure/EventBus/RabbitMqConnection.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using Serilog;
+
+namespace Zagejmi.Infrastructure.EventStore;
+
+public class RabbitMqConnection
+{
+    public static async Task<IConnection> CreateConnection(
+        string hostname = "localhost",
+        int port = 5672,
+        string username = "guest",
+        string password = "guest",
+        string virtualHost = "/")
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = hostname,
+            Port = AmqpTcpEndpoint.DefaultAmqpSslPort,
+            UserName = username,
+            Password = password,
+            VirtualHost = virtualHost,
+            AutomaticRecoveryEnabled = true,
+            NetworkRecoveryInterval = TimeSpan.FromSeconds(10)
+        };
+
+        try
+        {
+            Log.Information("Attempting to connect to RabbitMQ at {Hostname}:{Port} (vhost: {VirtualHost})",
+                hostname,
+                port,
+                virtualHost
+            );
+            IConnection connection = await factory.CreateConnectionAsync();
+            Log.Information("Connected to RabbitMQ successfully!");
+            return connection;
+        }
+        catch (Exception ex)
+        {
+            Log.Information(
+                "Could not connect to RabbitMQ: {ExMessage}",
+                ex.Message
+            );
+            throw;
+        }
+    }
+}
